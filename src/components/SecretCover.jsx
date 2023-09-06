e1b0a1d072e77e9507fa58c3a04b70d918e5d7a8
Edgar Carbajal <edgar99carbajal@gmail.com>
2023-09-05 20:56:44 -0700

    finished secret corner functionality

--- src/components/SecretCover.jsx
+++ src/components/SecretCover.jsx
@@ -2,13 +2,18 @@ import React from 'react'
 import '../styles/SecretCover.css'
 import ActivateButton from './ActivateButton';
 
+const reveal = () =>{
+  document.getElementById("secret-corner").style.opacity = 0;
+  document.getElementById("secret-corner").style.zIndex = -100;
+
+}
+
 
 function SecretCover(props) {
   return (
     <div className='secret-main'>
         <ActivateButton activated={props.activated} click={props.handleClick} ></ActivateButton>
-        <div className='secret-corner'></div>
-        {/* <div className='secret-cover'></div> */}
+        <div className='secret-corner' id='secret-corner' onClick={reveal}></div>
     </div>
   )
 }
